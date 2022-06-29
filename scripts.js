90f85e13ccca21067cdc72d0a88c506bafc000fa
Raul <raulvillajr95@gmail.com>
2022-06-28 23:20:46 -0700

    Add list of random words to be used

---
+++ scripts.js
@@ -0,0 +1,8 @@
+const mainWord = document.querySelector('.main-word')
+
+const words = ['October', 'Bite', 'Earthquake', 'Inflate', 'Coma', 'Sausage', 'Therapist', 'Satisfaction', 'Lack', 'Necklace', 'Folk', 'Regard']
+
+// Preset functions
+function randomNum(b) {
+  return Math.floor(Math.random() * b) + 1;
+}
